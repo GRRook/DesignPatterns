7295c5a5f48b5bab1e194764c81ec25becd6e65f
AdeZwart <andredezwart@gmail.com>
2016-06-13 16:41:09 +0200

    Added FactoryPattern to WinFormsGUI

---
+++ WinFormsGUI/Factory/ComponentFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsGUI.Decorator;
+
+namespace WinFormsGUI.Factory
+{
+	public class ComponentFactory
+	{
+		public BaseComponent GetComponent(string componentType)
+		{
+			componentType = componentType.ToLower();
+			var label = new ConcreteLabel();
+
+			switch (componentType)
+			{
+				case "Label":
+					return label;
+				case "button":
+					return new ButtonDecorator(label);
+				default:
+					return null;
+			}
+		}
+	}
+}
